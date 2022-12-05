7b55995844d09f482dd7ff42918136a922fbdd91
Sergio Sanchez <sergiosanchez.alu@iespacomolla.es>
2022-12-05 13:03:43 +0100

    Primera modificacion

---
+++ AreaCirculo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AreaCirculo {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Introduce el radio: ");
+        double r = teclado.nextDouble();
+        double area = (22 * r * r) / 7;
+        System.out.println("El area del circulo es: " + area);
+
+    }
+}
